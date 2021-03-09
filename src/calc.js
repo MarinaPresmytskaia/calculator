b203fcc2eac0501cf484a04d3c996f0e685ba8cb
Marina <mpresmytskaia@gmail.com>
2021-03-09 19:28:24 +0300

    upgrade code

--- src/calc.js
+++ src/calc.js
@@ -8,21 +8,35 @@ let memoryCurrentNumber = 0,
   memoryNewNumber = false,
   memoryPendingOperation = '';
 
-for (let i = 0; i < numbers.length; i++) {
+/*for (let i = 0; i < numbers.length; i++) {
   const number = numbers[i];
   number.addEventListener('click', (e) => numberPress(e.target.textContent));
-}
+}*/
+numbers.forEach(number => number.addEventListener('click', (e) => numberPress(e.target.textContent)));
 
-for (let i = 0; i < operators.length; i++) {
+/*for (let i = 0; i < operators.length; i++) {
   const operator = operators[i];
   operator.addEventListener('click', (e) => operation(e.target.textContent));
-}
+}*/
+operators.forEach(operator => operator.addEventListener('click', (e) => operation(e.target.textContent)));
 
 clearBtn.addEventListener('click', clear);
 
 decimalBtn.addEventListener('click', decimal);
 
-function numberPress(num) {
+/*function numberPress(num) {
+  if (memoryNewNumber) {
+    display.value = num;
+    memoryNewNumber = false;
+  } else {
+    if (display.value === '0') {
+      display.value = num;
+    } else {
+      display.value += num;
+    }
+  }
+}*/
+const numberPress = num => {
   if (memoryNewNumber) {
     display.value = num;
     memoryNewNumber = false;
@@ -35,7 +49,7 @@ function numberPress(num) {
   }
 }
 
-function operation(oper) {
+/*function operation(oper) {
   let localOperationMemory = display.value;
 
   if (memoryNewNumber && memoryPendingOperation !== '=') {
@@ -56,6 +70,29 @@ function operation(oper) {
   }
   display.value = memoryCurrentNumber;
   memoryPendingOperation = oper;
+}*/
+
+const operation = oper => {
+  let localOperationMemory = display.value;
+
+  if (memoryNewNumber && memoryPendingOperation !== '=') {
+    display.value = memoryCurrentNumber;
+  } else {
+    memoryNewNumber = true;
+    if (memoryPendingOperation === '+') {
+      memoryCurrentNumber += parseFloat(localOperationMemory);
+    } else if (memoryPendingOperation === '-') {
+      memoryCurrentNumber -= parseFloat(localOperationMemory);
+    } else if (memoryPendingOperation === '*') {
+      memoryCurrentNumber *= parseFloat(localOperationMemory);
+    } else if (memoryPendingOperation === '/') {
+      memoryCurrentNumber /= parseFloat(localOperationMemory);
+    } else {
+      memoryCurrentNumber = parseFloat(localOperationMemory);
+    }
+  }
+  display.value = memoryCurrentNumber;
+  memoryPendingOperation = oper; 
 }
 
 function decimal(arg) {
